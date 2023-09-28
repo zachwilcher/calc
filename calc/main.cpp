4d823944af2c49139f3b707510d0081fa9258db9
zachwilcher <mail@zachwilcher.com>
2023-09-28 11:35:30 -0400

    Initial Commit

---
+++ calc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  calc
+//
+//  Created by Zach Wilcher on 9/28/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
